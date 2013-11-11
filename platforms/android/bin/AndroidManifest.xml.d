631c0192030970482bc4933790882bcb84b5c0d0
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-11-11 21:40:10 +1100

    Clean out

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/tommy/devgeeks/devgeeks/Canvas2ImageDemo/platforms/android/bin/AndroidManifest.xml : \
-/Users/tommy/devgeeks/devgeeks/Canvas2ImageDemo/platforms/android/AndroidManifest.xml \
--- platforms/android/gen/org/devgeeks/canvas2imageplugindemo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.devgeeks.canvas2imageplugindemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
