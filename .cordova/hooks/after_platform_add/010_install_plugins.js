60e557a2af0ae3f9f867a1f117e54c4e5da2ab4e
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-11-19 15:43:33 +1100

    Added hook to install plugins

---
+++ .cordova/hooks/after_platform_add/010_install_plugins.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+ 
+//this hook installs all your plugins
+ 
+// add your plugins to this list--either 
+// the identifier, the filesystem location 
+// or the URL
+var pluginlist = [
+  'org.apache.cordova.device',
+  'org.apache.cordova.dialogs',
+  'org.devgeeks.Canvas2ImagePlugin'
+];
+ 
+// no need to configure below
+ 
+var fs = require('fs');
+var path = require('path');
+var sys = require('sys');
+var exec = require('child_process').exec;
+ 
+function puts(error, stdout, stderr) {
+  sys.puts(stdout);
+}
+ 
+pluginlist.forEach(function(plug) {
+  exec("cordova plugin add " + plug, puts);
+});
